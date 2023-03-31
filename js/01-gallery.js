d0f36f5bdc8c9c6182e7ce97e1720565dc46a109
SirotovAlexander <alexander.sirotov@gmail.com>
2023-04-01 00:37:01 +0300

    done-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryUl = document.querySelector(".gallery");
+
+let markup = "";
+for (let i = 0; i < galleryItems.length; i += 1) {
+  markup += `<li class="gallery__item">
+  <a class="gallery__link" href="${galleryItems[i].original}">
+    <img
+      class="gallery__image"
+      src="${galleryItems[i].preview}"
+      data-source="${galleryItems[i].original}"
+      alt="${galleryItems[i].description}"
+    />
+  </a>
+</li>`;
+}
+
+galleryUl.insertAdjacentHTML("afterbegin", markup);
