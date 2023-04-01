72aedc93fedafe2b259c069b6f87a1b143318b37
SirotovAlexander <alexander.sirotov@gmail.com>
2023-04-01 23:01:06 +0300

    add-some-fith
    add-some-fith

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,14 +1,14 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-// console.log(galleryItems);
-
 const galleryUl = document.querySelector(".gallery");
+galleryUl.addEventListener("click", onClick);
+galleryUl.addEventListener("keyup", onClick);
 
 let markup = "";
 for (let i = 0; i < galleryItems.length; i += 1) {
   markup += `<li class="gallery__item">
-  <a class="gallery__link" href="${galleryItems[i].original}">
+  <a class="gallery__link" href="${galleryItems[i].original}" target="_blank" rel="noreferrer noopener">
     <img
       class="gallery__image"
       src="${galleryItems[i].preview}"
@@ -20,3 +20,28 @@ for (let i = 0; i < galleryItems.length; i += 1) {
 }
 
 galleryUl.insertAdjacentHTML("afterbegin", markup);
+
+function onClick(event) {
+  console.log(event.target.dataset.source);
+
+  const instance = basicLightbox.create(`
+	<img
+      class="gallery__image"
+      src="${event.target.dataset.source}"
+      data-source="${event.target.dataset.source}"
+    />
+`);
+
+  event.preventDefault();
+  instance.show();
+
+  if (event.code === "Escape") {
+    instance.close();
+  }
+}
+
+function onKey(event) {
+  if (event.code === "Escape") {
+    instance.close();
+  }
+}
