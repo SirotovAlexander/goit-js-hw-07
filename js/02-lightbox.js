a48adb9443e06e84d6a4ef20c2e53ab9aa51a8a5
SirotovAlexander <alexander.sirotov@gmail.com>
2023-04-02 20:51:01 +0300

    work-on-second
    work-on-second

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryGrid = document.querySelector(".gallery");
+
+let markup = "";
+
+for (let i = 0; i < galleryItems.length; i += 1) {
+  markup += `<li class="gallery__item">
+   <a class="gallery__link" href="${galleryItems[i].original}">
+      <img class="gallery__image" src="${galleryItems[i].preview}" alt="${galleryItems[i].description}" />
+   </a>
+</li>`;
+}
+
+galleryGrid.insertAdjacentHTML("afterbegin", markup);
+
+let gallery = new SimpleLightbox(".gallery a");
+console.log(gallery);
